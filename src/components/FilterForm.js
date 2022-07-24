2553c008985c87a45127bf6497e8e20206ba0d07
Berke Anday Baloğlu <andyanday33@gmail.com>
2022-07-24 04:53:25 +0100

    More comments

--- src/components/FilterForm.js
+++ src/components/FilterForm.js
@@ -38,12 +38,14 @@ export default function FilterForm({ setFilteredData, unfilteredDataRef }) {
 return (
   <section className="filter-form">
     <h2 style={{gridColumn: '1 / span 2', textAlign: 'center'}}>Filter Data</h2>
+    {/* Start and End date filter */}
     <section className="date-filter">
       <label>Start Date</label>
       <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
       <label>End Date</label>
       <DatePicker selected={endDate} onChange={(date) => setEndDate(date)} />
     </section>
+    {/* Minimum ABV Filter */}
     <section className="ABV-filter">
       <label>ABV:</label>
       <input type="number" onChange={handleABVFilterChange} />
