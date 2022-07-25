065c56de808f345cacd20f86536a81c6b290e179
Berke Anday Baloğlu <andyanday33@gmail.com>
2022-07-25 10:26:11 +0100

    Fix: multiple filters were not being applied at the same time

--- src/components/FilterForm.js
+++ src/components/FilterForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import DatePicker from 'react-datepicker';
 
 import "react-datepicker/dist/react-datepicker.css";
@@ -15,13 +15,23 @@ import "react-datepicker/dist/react-datepicker.css";
 export default function FilterForm({ setFilteredData, unfilteredDataRef }) {
   const [startDate, setStartDate] = useState(new Date('Jan 2004'));
   const [endDate, setEndDate] = useState(new Date()); // Today's date
+  const ABVRef = useRef(null);
 
   useEffect(() => {
     if (unfilteredDataRef.current && unfilteredDataRef.current.length > 0) {
-      const filteredData = unfilteredDataRef.current.filter(beer => {
+      // Date filtering
+      let filteredData = unfilteredDataRef.current.filter(beer => {
         const date = new Date(beer.date);
         return date >= startDate && date <= endDate;
       });
+      // ABV filtering
+      let abv = ABVRef?.current?.value;
+      if(abv) {
+        filteredData = filteredData.filter(beer => {
+          return +beer.abv >= +abv;
+        });
+      }
+      
       setFilteredData(filteredData);
     }
 
@@ -29,8 +39,14 @@ export default function FilterForm({ setFilteredData, unfilteredDataRef }) {
 
   const handleABVFilterChange = (e) => {
     let abv = e.target.value;
-    const filteredData = unfilteredDataRef.current.filter(beer => {
+    // ABV filtering
+    let filteredData = unfilteredDataRef.current.filter(beer => {
       return +beer.abv >= +abv;
+    });
+    // Date filtering
+    filteredData = filteredData.filter(beer => {
+      const date = new Date(beer.date);
+      return date >= startDate && date <= endDate;
     });
     setFilteredData(filteredData);
   }
@@ -48,7 +64,7 @@ return (
     {/* Minimum ABV Filter */}
     <section className="ABV-filter">
       <label>ABV:</label>
-      <input type="number" onChange={handleABVFilterChange} />
+      <input ref={ABVRef} type="number" onChange={handleABVFilterChange} />
     </section>
       
   </section>
